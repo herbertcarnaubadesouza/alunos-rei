fabbb0dcc5a77c7c402c79a92cc63056f2dad600
Herbert Carnauba de Souza <herbertcarnaubadesouza@gmail.com>
2021-11-06 09:22:42 -0300

    Continuo Fazendo a tela do carrinho

--- COMPRA/js/script.js
+++ COMPRA/js/script.js
@@ -1,45 +1,51 @@
-// window.onload = function(){
-//     MontarCarrinho();
-// }  
+window.onload = function(){
+    MontarCarrinho();
+}  
 
 
-// function MontarCarrinho(){
-//     var caixa = JSON.parse(window.localStorage.getItem("carrinho"));
+function MontarCarrinho(){
+    var caixa = JSON.parse(window.localStorage.getItem("carrinho"));
 
-//     document.getElementById("tabelaCarrinho").innerHTML = "";
+    document.getElementById("tabelaCarrinho").innerHTML = "";
+    document.getElementById("divTitulo").innerHTML = "";
 
-//     var conteudo = ""
-//     conteudo += '<table>';
-//     conteudo += '<tr>';
-//     conteudo += '<td>' + 'Imagem do produto' + '</td>';
-//     conteudo += '<td>' + 'Você está comprando' + '</td>';
-//     conteudo += '<td>' + 'Unidades' + '</td>';
-//     conteudo += '<td>' + 'Preço (R$)' + '</td>';
-//     conteudo += '<td>' + 'Total (R$)' + '</td>';
-//     conteudo += '</tr>';
-//     document.getElementById("tabelaCarrinho").innerHTML += conteudo;
-//     if(caixa != null){
-//         for(var i = 0; i < caixa.length; i++){
-//             conteudo = ""
-//             conteudo += '<tr>';
-//             conteudo += '<td>';
-//             conteudo += '<img src="CSS/' + caixa[i][2] + '"/>';
-//             conteudo += '</td>';
-//             conteudo += '<td>';
-//             conteudo += caixa[i][1];
-//             conteudo += '</td>';
-//             conteudo += '<td>';
-//             conteudo += caixa[i][4];
-//             conteudo += '</td>';
-//             conteudo += '<td>';
-//             conteudo += caixa[i][5];
-//             conteudo += '</td>';
-//             conteudo += '<td>';
-//             conteudo += caixa[i][5]*caixa[i][4];
-//             conteudo += '</td>';
-//             conteudo += '</tr>';
-//         }
-//     }
-//     document.getElementById("tabelaCarrinho").innerHTML += conteudo;
+    var titulo = "";
+    var conteudo = ""
+    if(caixa != null){
 
-// }
+        titulo +='<h2 class ="Titulo" data-text="Carrinho...">Carrinho...</h2>'
+        conteudo += '<table>';
+        conteudo += '<tr>';
+        conteudo += '<td>' + 'Imagem do produto' + '</td>';
+        conteudo += '<td>' + 'Você está comprando' + '</td>';   
+        conteudo += '<td>' + 'Preço (R$)' + '</td>';
+        conteudo += '<td>' + 'Total (R$)' + '</td>';
+        conteudo += '</tr>';
+        document.getElementById("tabelaCarrinho").innerHTML += conteudo;    
+        for(var i = 0; i < caixa.length; i++){
+            conteudo = ""
+            conteudo += '<tr>';
+            conteudo += '<td>';
+            conteudo += `<img src="img/${caixa[i][2]}" class="photo"/>`;
+            conteudo += '</td>';
+            conteudo += '<td>';
+            conteudo += caixa[i][1];
+            conteudo += '</td>';            
+            conteudo += '<td>';
+            conteudo += caixa[i][4];
+            conteudo += '</td>';
+            conteudo += '<td>';
+            conteudo += caixa[i][4]*caixa[i][4];
+            conteudo += '</td>';
+            conteudo += '</tr>';
+            conteudo += '</table>';
+        }
+        
+    }
+    else{
+        titulo +='<h2 class ="Titulo-vazio" data-text="Carrinho vazio...">Carrinho vazio...</h2>'
+    }
+    document.getElementById("divTitulo").innerHTML += titulo;
+    document.getElementById("tabelaCarrinho").innerHTML += conteudo;
+
+}
