e9e9d5ad315e4e22ac0a93a9434d04123af7136e
herbertraizen <Credicar05@>
2023-01-17 12:38:40 -0300

    first commit

--- LOGIN/JS/script.js
+++ LOGIN/JS/script.js
@@ -1,15 +1,14 @@
-
-
 const formulario = document.getElementById("formLogin");
-formulario.addEventListener('submit',clickLogin);
+formulario.addEventListener("submit", clickLogin);
 
-function clickLogin(event){
-    event.preventDefault();    
-    if (document.getElementById("inputLogin").value == "" ||
-            document.getElementById("inputSenha").value == "" ){
-        alert("Preencha todos os campos");
-    }else{
-        document.location.href = "../PRINCIPAL"
-    }
-
+function clickLogin(event) {
+  event.preventDefault();
+  if (
+    document.getElementById("inputLogin").value == "" ||
+    document.getElementById("inputSenha").value == ""
+  ) {
+    alert("Preencha todos os campos");
+  } else {
+    document.location.href = "..";
+  }
 }
